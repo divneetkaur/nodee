fe5975c338c98d0a0812089684665560d1333f77
divneetkaur <79521942+divneetkaur@users.noreply.github.com>
2023-09-20 22:54:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Signup from './components/Signup/Signup';
+import Login from './components/Login/Login';
+
+function App() {
+  return (
+    <div>
+      <Login/>
+      
+    </div>
+  );
+}
+
+export default App;
